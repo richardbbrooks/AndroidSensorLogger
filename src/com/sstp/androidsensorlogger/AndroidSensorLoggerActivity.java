8424dc38b89993f3566d1518d92e7d4654350fca
Benji Englard <benjienglard@yahoo.com>
2011-07-23 18:08:50 -0400

    Text to Speech now speaks longitude, latitude, and altitude data

--- src/com/sstp/androidsensorlogger/AndroidSensorLoggerActivity.java
+++ src/com/sstp/androidsensorlogger/AndroidSensorLoggerActivity.java
@@ -9,10 +9,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
 import android.app.Activity;
@@ -44,21 +44,23 @@ public class AndroidSensorLoggerActivity extends Activity implements LocationLis
         tv.setText("Initializing...");
         mTts = new TextToSpeech(this, this);
     }
+    
 
     public void onLocationChanged(Location arg0) {
         String lat = String.valueOf(arg0.getLatitude()); //lat is retrieved 
         String lon = String.valueOf(arg0.getLongitude()); //lon is retrieved
-        tv.setText("lat="+lat+", lon="+lon);
+        String alt = String.valueOf(arg0.getAltitude()); //alt is retrieved
+        tv.setText("lat="+lat+", lon="+lon + ", alt="+alt);
         SimpleDateFormat utc = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
         utc.setTimeZone(TimeZone.getTimeZone("UTC"));
         String utcTime = utc.format(new Date());
         try {
-			// Writes the lat and lon every time a new location is observed.
-        	SaveData(lat, lon, utcTime, "LocationData.csv");
+			// Writes the lat, lon, and alt every time a new location is observed.
+        	SaveData(lat, lon, alt, utcTime, "LocationData.csv");
         	// Speaks out new line
         	speakData(new File(Environment.getExternalStorageDirectory()+"/LocationData.csv"));
-        	
-		} catch (Exception e) {
+
+        } catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -73,7 +75,7 @@ public class AndroidSensorLoggerActivity extends Activity implements LocationLis
         Log.e("GPS", "status changed to " + arg0 + " [" + arg1 + "]");
     }
      
-    public void SaveData(String lat, String lon, String time, String fileLocation) throws Exception
+    public void SaveData(String lat, String lon, String alt, String time, String fileLocation) throws Exception
 	{
     	
     	// **SIDE NOTE 2: In order to write/read into the SD card, a permission script has to be added to the 
@@ -85,7 +87,7 @@ public class AndroidSensorLoggerActivity extends Activity implements LocationLis
 		        File gpxfile = new File(root, fileLocation);
 		        FileWriter gpxwriter = new FileWriter(gpxfile, true);
 		        CSVWriter out = new CSVWriter(gpxwriter);
-		        String[] log = new String[] {lat, lon, time};
+		        String[] log = new String[] {lat, lon, alt, time};
 		        out.writeNext(log);
 		        out.flush();
 		       
@@ -116,9 +118,9 @@ public class AndroidSensorLoggerActivity extends Activity implements LocationLis
                // Lanuage data is missing or the language is not supported.
                 Log.e(TAG, "Language is not available.");
             } else {
-    			mTts.speak("The following audio is GPS coordinates " +
-    						"from an android phone attached to a balloon made by Team SWAG!", 
-    						TextToSpeech.QUEUE_FLUSH, null);
+    			//mTts.speak("The following audio is GPS coordinates " +
+    				//		"from an android phone attached to a balloon made by Team SWAG!", 
+    					//	TextToSpeech.QUEUE_FLUSH, null);
             }
         } else {
             // Initialization failed.
@@ -131,12 +133,30 @@ public class AndroidSensorLoggerActivity extends Activity implements LocationLis
     	Scanner in = null;
 		try {
 			in = new Scanner(f);
-		} catch (FileNotFoundException e) {
+		} 
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-    	String s = "";
-    	while (in.hasNextLine())
-    		s = in.nextLine(); // overwrites s so always speaks last line (the new data)
-    	mTts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+		if (!mTts.isSpeaking()) {
+	    	String s = "";
+	    	String la = "Latitude";
+	    	String lo = "Longitude";
+	    	String al = "Altitude";
+	    	String text = "";
+	    	while (in.hasNextLine())
+	    		s = in.nextLine();  	// s is always assigned to last (newest) line
+	    	int posSpace = s.indexOf(",");
+	    	String lat = s.substring(0,posSpace);
+	    	s = s.replace(s.substring(0,posSpace) + ",", "");
+	    	posSpace = s.indexOf(",");
+	    	String lon = s.substring(0, posSpace);
+	    	s = s.replace(s.substring(0,posSpace) + ",", "");
+	    	posSpace = s.indexOf(",");
+	    	String alt = s.substring(0, posSpace);
+	    	// Speaks "Longitude", "Lon. data", "Latitude", "Lat. data", "Altitude", "Alt. data"
+	    	text = la + lat + lo + lon + al + alt;
+	    	mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+		}
+		else {}
     }
 }
