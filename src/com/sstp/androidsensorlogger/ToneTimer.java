f4ab6d793119d299c48b59e1e1635958951c561e
Benji Englard <benjienglard@yahoo.com>
2011-07-27 22:36:54 -0400

    Added so that plays tone after GPS service lost

--- src/com/sstp/androidsensorlogger/ToneTimer.java
+++ src/com/sstp/androidsensorlogger/ToneTimer.java
@@ -7,9 +7,15 @@ public class ToneTimer extends TimerTask {
 	
 	ToneGenerator tg = null;
 
-	@Override
 	public void run() {
+		AndroidSensorLoggerActivity asl = new AndroidSensorLoggerActivity();
+		while (true) {
+	    	if (asl.getCurrentUTCTime().compareTo(asl.getUTCTime()) >= 30 || 
+	    			asl.getCurrentUTCTime().compareTo(asl.getUTCTime()) == 1)
+	    		break;
+	    	else {}
+		}
 		tg = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-		tg.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT);
+		tg.startTone(ToneGenerator.TONE_CDMA_HIGH_PBX_SLS);
 	}
 }
